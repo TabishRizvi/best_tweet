21b87d3af63ec9129a422b890108dc64e6f929db
Tabish Rizvi <tabish.haidar@clicklabs.in>
2016-06-09 12:22:59 +0530

    second

--- config.js
+++ config.js
@@ -8,7 +8,9 @@ module.exports = {
     port : 3001,
     twitter : {
         key : "1i3ZvThvxyU6heMhO5lMC2Em4",
-        secret : "	BA42d3jrfzxuMtjvbfVfT847o68AglOy74KD8IEODEOCZNxyv2"
+        secret : "BA42d3jrfzxuMtjvbfVfT847o68AglOy74KD8IEODEOCZNxyv2",
+        baseUrl : "https://api.twitter.com",
+        apiVersion : "1.1"
     }
 };
 
