5044c9b9fd80bbaf4158add5322f272cb86d752d
TabishRizvi <sayyidtabish@gmail.com>
2016-06-27 21:27:03 +0530

    removed keys

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ module.exports = {
 
     port : 3001,
     twitter : {
-        key : "1i3ZvThvxyU6heMhO5lMC2Em4",
-        secret : "BA42d3jrfzxuMtjvbfVfT847o68AglOy74KD8IEODEOCZNxyv2",
+        key : "****************",
+        secret : "***************************",
         baseUrl : "https://api.twitter.com",
         apiVersion : "1.1"
     }
